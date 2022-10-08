1a04ab522f930e5f2a1bc3dadc18ed2f07e31f98
Your Name <kalaitzidispaul@gmail.com>
2022-10-08 15:50:38 +0200

    set up project

---
+++ my-app/src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import {Button, Container, Nav, Navbar as NavB } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import {BiShoppingBag} from 'react-icons/bi'
+
+const Navbar = () => {
+  return (
+    <NavB className='bg-white shadow-sm mb-3' sticky="top">
+<Container>
+    <Nav className='me-auto'>
+        <Nav.Link to={"/"} as={NavLink}>Home</Nav.Link>
+        <Nav.Link to={"/store"} as={NavLink}>Store</Nav.Link>
+        <Nav.Link to={"/about"} as={NavLink}>About</Nav.Link>
+    </Nav>
+   <Button variant='outline-primary' style={{width: "3rem", height: "3rem", display: "flex", position: "relative"}}><BiShoppingBag style={{width: "2rem", height: "2rem"}}/><span className='rounded-circle bg-danger d-flex justify-content-center align-items-center' style={{color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%" }}></span></Button>
+</Container>
+    </NavB>
+  )
+}
+
+export default Navbar
